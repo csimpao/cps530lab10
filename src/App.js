f72b2a64a51183a26ae9d161be49a77666cc9f5c
csimpao <csimpao@torontomu.ca>
2025-11-25 10:26:56 -0500

    530lab10 commit #5

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
@@ -7,11 +6,12 @@ function App() {
       <h1>Problem 2: Frameworks</h1>
 
       <h2>Installation + Setup</h2>
-      <ol>
-        <li>Since I have a Mac, React is already installed</li>
-        <li>I had to create a React app in order to do this lab: npx create-react-app 530lab10</li>
-        <li>Then, I used Vercel as my host, in order to run the app with React</li>
-      </ol>
+      
+      <p>Since I have a Mac, React is already installed</p> 
+      <p>I had to create a React app in order to do this lab: npx create-react-app 530lab10</p>
+      <p>Then, I used Vercel as my host, in order to run the app with React</p>
+
+      <h2>Building the Page with React</h2>
     </div>
   );
 }
